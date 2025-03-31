6f1bbe2f288cce03ac4627da2e7c1d24e31ee0e2
mereluo <meredithluo710@gmail.com>
2025-03-31 11:39:31 -0700

    Finish exercises

---
+++ README.md
@@ -0,0 +1,47 @@
+# Project 5 - *Tumblr Posts*
+
+Submitted by: **Meredith Luo**
+
+**Tumblr Posts** is an app that displays tumblr posts in a table view
+
+Time spent: **2** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] App has a configured table view and table view call
+- [x] App populates the table view with data fetched from an API
+
+The following **optional** features are implemented:
+
+- [x] App fetches posts from a different Tumblr blog
+- [x] App has a refresh control to update the table view
+
+## Video Walkthrough
+
+<div>
+    <a href="https://www.loom.com/share/3612c9b9a0db41de815f73ceec3b84bc">
+      <img style="max-width:300px;" src="https://cdn.loom.com/sessions/thumbnails/3612c9b9a0db41de815f73ceec3b84bc-53162520ce0d39c3-full-play.gif">
+    </a>
+  </div>
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright [2025] [Meredith Luo]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -6,50 +6,92 @@
 import UIKit
 import Nuke
 
-class ViewController: UIViewController {
-
-
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        
+        let post = posts[indexPath.row]
+        
+        if let photo = post.photos.first {
+            let imageUrl = photo.originalSize.url
+            Nuke.loadImage(with: imageUrl, into: cell.postImageView)
+        }
+        
+        cell.summaryLabel.text = post.summary
+        
+        return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    private var posts: [Post] = []
+    private var blogNames = ["humansofnewyork", "humansofseoul", "humansofparis"]
+    private var currentBlogIndex = 0
+    private let refreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
         
-        fetchPosts()
+        // set up pull to refresh
+        refreshControl.addTarget(self, action: #selector(didPullToRefresh), for: .valueChanged)
+        tableView.refreshControl = refreshControl
+        
+        fetchPosts(from: blogNames[currentBlogIndex])
     }
-
-
-
-    func fetchPosts() {
-        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
+    
+    @objc func didPullToRefresh() {
+        // Move to the next blog (circularly)
+        currentBlogIndex = (currentBlogIndex + 1) % blogNames.count
+        let nextBlog = blogNames[currentBlogIndex]
+        
+        print("🔁 Refreshing from: \(nextBlog)")
+        fetchPosts(from: nextBlog)
+    }
+    
+    
+    func fetchPosts(from blog: String) {
+        let url = URL(string: "https://api.tumblr.com/v2/blog/\(blog)/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
         let session = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            DispatchQueue.main.async { [weak self] in
+                // Always stop refreshing
+                self?.refreshControl.endRefreshing()
+            }
+            
             if let error = error {
                 print("❌ Error: \(error.localizedDescription)")
                 return
             }
-
+            
             guard let statusCode = (response as? HTTPURLResponse)?.statusCode, (200...299).contains(statusCode) else {
                 print("❌ Response error: \(String(describing: response))")
                 return
             }
-
+            
             guard let data = data else {
                 print("❌ Data is NIL")
                 return
             }
-
+            
             do {
                 let blog = try JSONDecoder().decode(Blog.self, from: data)
-
+                
                 DispatchQueue.main.async { [weak self] in
-
+                    
                     let posts = blog.response.posts
-
-
+                    self?.posts = posts
+                    self?.tableView.reloadData()
+                    
                     print("✅ We got \(posts.count) posts!")
                     for post in posts {
                         print("🍏 Summary: \(post.summary)")
                     }
                 }
-
+                
             } catch {
                 print("❌ Error decoding JSON: \(error.localizedDescription)")
             }
